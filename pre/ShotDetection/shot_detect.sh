0dfe24c898f1a6697f4935d4c6fe91d31e6226cb
digbose92 <digbose92@gmail.com>
2023-12-14 03:45:53 -0800

    commit in SceneSeg

--- pre/ShotDetection/shot_detect.sh
+++ pre/ShotDetection/shot_detect.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=59GB
-#SBATCH --time=3:00:00
-#SBATCH --partition=debug
+#SBATCH --cpus-per-task=64
+#SBATCH --time=48:00:00
+#SBATCH --partition=epyc-64
 #SBATCH --account=shrikann_35
 
 module load gcc/11.3.0
+module load libxext/1.3.4
+module load ffmpeg/6.0
 eval "$(conda shell.bash hook)"
 conda activate /project/shrikann_35/dbose/envs/vlm_sem_seg
 
-cd /project/shrikann_35/dbose/codes/semantic_scene_segmentation/VLM-Sem-Seg/SceneSeg/pre/ShotDetection
-python shotdetect_p.py --num_workers 4 --source_path /scratch1/rajatheb/condensed_movies/videos_hq --list_file /project/shrikann_35/dbose/codes/semantic_scene_segmentation/VLM-Sem-Seg/data/Condensed_movies.txt --save_data_root_path /scratch1/dbose/video_scene_boundary_detection/Condensed_Movies_shots --split_video True
+cd /project/shrikann_35/dbose/codes/video_scene_boundary_detection/VLM-Sem-Seg/SceneSeg/pre/ShotDetection
+python shotdetect_p.py --num_workers 8 --list_file /project/shrikann_35/dbose/codes/video_scene_boundary_detection/VLM-Sem-Seg/file_shards/remaining_file_path_list.txt --save_data_root_path /scratch1/dbose/video_scene_boundary_detection/Condensed_Movies_shots/SceneSeg --split_video
